307f73a3dd76d65d7053db5fd3b2dbb9300fead1
Craig McDaniel <craig.mcdaniel@db.com>
2010-11-18 11:17:44 -0500

    Initial commit and project setup.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojureve "1.0.0-SNAPSHOT"
+  :description "A Clojure wrapper for the Eve Online API"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/clojureve/test/core.clj
@@ -0,0 +1,8 @@
+(ns eveapi.test.core
+  (:use [eveapi.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
+
+
