a3ef70b58a39c7f476896026ff53ed0cf4480c1a
Craig McDaniel <cpmcdaniel@gmail.com>
2010-11-18 23:57:57 -0500

    Added more character API calls

--- src/clojureve/api.clj
+++ src/clojureve/api.clj
@@ -1,5 +1,6 @@
 (ns clojureve.api
-  (:require [clojure.contrib.http.agent :as http]))
+  (:require [clojure.contrib.http.agent :as http])
+  (:require [clojure.string]))
 
 (def *api-server* "http://api.eve-online.com")
 
@@ -7,19 +8,30 @@
 (defn make-url
   ([uri]
      (str *api-server* uri))
-  ([uri user-id api-key]
-     (str (make-url uri) "?userId=" user-id "&apiKey=" api-key))
-  ([uri user-id api-key character-id]
-     (str (make-url uri user-id api-key) "&characterId=" character-id)))
+  ([uri params]
+     (str (make-url uri) "?"
+	  (clojure.string/join "&"
+	   (map (fn [[k v]]
+		  (str k "=" v))
+		params)))))
 
 (defn- anonymous-call [uri]
-  (http/stream (http/http-agent (make-url uri))))
+  (http/stream
+   (http/http-agent
+    (make-url uri))))
 
 (defn- account-call [uri user-id api-key]
-  (http/stream (http/http-agent (make-url uri user-id api-key))))
+  (http/stream
+   (http/http-agent
+    (make-url uri {"userId" user-id
+		   "apiKey" api-key}))))
 
 (defn- character-call [uri user-id api-key character-id]
-  (http/stream (http/http-agent (make-url uri user-id api-key character-id))))
+  (http/stream
+   (http/http-agent
+    (make-url uri {"userId" user-id
+		   "apiKey" api-key
+		   "characterId" character-id}))))
 
 ;; Anonymous calls. These are all no-args functions.
 (def server-status
@@ -32,10 +44,60 @@
 ;; Account calls.
 (def character-list
      (partial account-call "/account/CharacterList.xml.aspx"))
+(def account-status
+     (partial account-call "/account/AccountStatus.xml.aspx"))
 
 ;; Character calls.
+(def account-balance
+     (partial character-call "/char/AccountBalance.xml.aspx"))
+(def asset-list
+     (partial character-call "/char/AssetList.xml.aspx"))
+(def calendar-event-attendees
+     (partial character-call "/car/CalendarEventAttendees.xml.aspx"))
 (def character-sheet
      (partial character-call "/char/CharacterSheet.xml.aspx"))
+(def contact-list
+     (partial character-call "/char/ContactList.xml.aspx"))
+(def contact-notifications
+     (partial character-call "/char/ContactNotifications.xmlaspx"))
+(def factional-warfare-stats
+     (partial character-call "/char/FacWarStats.xml.aspx"))
+(def industry-jobs
+     (partial character-call "/char/IndustryJobs.xml.aspx"))
+(def mailing-lists
+     (partial character-call "/char/MailingLists.xml.aspx"))
+(def mail-messages
+     (partial character-call "/char/MailMessages.xml.aspx"))
+(def market-orders
+     (partial character-call "/char/MarketOrders.xml.aspx"))
+(def medals
+     (partial character-call "/char/Medals.xml.aspx"))
+(def notifications
+     (partial character-call "/char/Notifications.xml.aspx"))
+(def standings
+     (partial character-call "/char/Standings.xml.aspx"))
+(def research
+     (partial character-call "/char/Research.xml.aspx"))
+(def skill-in-training
+     (partial character-call "/char/SkillInTraining.xml.aspx"))
+(def skill-queue
+     (partial character-call "/char/SkillQueue.xml.aspx"))
+(def upcoming-calendar-events
+     (partial character-call "/char/UpcomingCalendarEvents.xml.aspx"))
+
+
+;; Optional param beforeKillId
+(def kill-log)
+
+;; ids param - comma separated list of messageIds from MailMessages.
+(def mail-bodies)
+
+;; beforeRefId - used to walk the journal backwards in time.
+(def wallet-journal)
+
+;; beforeTransId - see wallet-journal about "walking".
+(def wallet-transactions)
 
 
 
+;; Corporation calls.
--- test/clojureve/test/api.clj
+++ test/clojureve/test/api.clj
@@ -1,4 +1,5 @@
 (ns clojureve.test.api
+  (:require [clojure.xml :as xml])
   (:use [clojureve.api] :reload)
   (:use [clojure.test]))
 
@@ -6,13 +7,12 @@
 (deftest url-construction
   (is (= "http://api.eve-online.com/eve/SkillTree.xml.aspx"
 	 (make-url "/eve/SkillTree.xml.aspx"))
-      "make-url failed for anonymous call")
+      "make-url failed for parameter-less call")
   (is (= "http://api.eve-online.com/account/Characters.xml.aspx?userId=jim&apiKey=ak"
-	 (make-url "/account/Characters.xml.aspx" "jim" "ak"))
-      "make-url failed for account call")
-  (is (= "http://api.eve-online.com/char/AccountBalance.xml.aspx?userId=jim&apiKey=ak&characterId=1"
-	 (make-url "/char/AccountBalance.xml.aspx" "jim" "ak" "1"))
-      "make-url failed for character call"))
+	 (make-url "/account/Characters.xml.aspx" {"userId" "jim" "apiKey" "ak"}))
+      "make-url failed for call with parameters"))
+
+
 
 
 
