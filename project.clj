e068af4ca8868ef9052365424fbdef2d8c0024d7
Craig McDaniel <cpmcdaniel@gmail.com>
2013-05-27 14:15:53 -0400

    Removing the kitchen sink. Focusing on the API itself for now.

--- project.clj
+++ project.clj
@@ -2,17 +2,7 @@
   :description "A Clojure wrapper for the Eve Online API"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
-		 [org.apache.camel/camel-http "2.5.0"]
-                 [mysql/mysql-connector-java "5.1.18"]
-                 [clj-http "0.7.2"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.3"]
-                 [org.zmq/jzmq "3.2.2"]]
-  :dev-dependencies [[lein-ring "0.8.5"]]
-  :native-path "/usr/lib"
-  :ring {:handler clojureve.core/app}
-  ;:main clojureve.main
-  ;:aot [clojureve.main]
+                 [clj-http "0.7.2"]]
 
   :test-selectors {:default (complement :integration)
                    :integration :integration
--- src/clojureve/main.clj
+++
@@ -1,8 +0,0 @@
-(ns clojureve.main
-  (:gen-class)
-  (:require [clojureve.zeromq :as zmq]))
-
-#_(:use [clojureve.setup :as setup ])
-
-(defn -main [& args]
-  (zmq/example))
